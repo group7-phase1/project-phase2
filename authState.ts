5b9d821626421b91075f8c0475f14a90986baf49
mahamgodil <mahamgodil123@gmail.com>
2023-10-16 13:45:33 -0400

    Check for Authentication

---
+++ authState.ts
@@ -0,0 +1,27 @@
+// Initial state
+let globalState = {
+    isAuthenticated: false,
+    username: '',
+};
+
+// Function to set authentication state
+function setAuthenticationState(isAuthenticated, username) {
+    globalState.isAuthenticated = isAuthenticated;
+    globalState.username = username;
+}
+
+// Function to check if a user is authenticated
+function checkAuthentication() {
+    return globalState.isAuthenticated;
+}
+
+// Function to get the username
+function getUsername() {
+    return globalState.username;
+}
+
+module.exports = {
+    setAuthenticationState,
+    checkAuthentication,
+    getUsername,
+};
