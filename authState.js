7019398ea2f04de07b46182423740bb0bd5a9183
RomanczuG <romanczug@icloud.com>
2023-10-16 13:58:19 -0400

    Created flow with authentication

--- authState.js
+++ authState.js
@@ -1,3 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.getUsername = exports.checkAuthentication = exports.setAuthenticationState = void 0;
 // Initial state
 var globalState = {
     isAuthenticated: false,
@@ -8,14 +11,17 @@ function setAuthenticationState(isAuthenticated, username) {
     globalState.isAuthenticated = isAuthenticated;
     globalState.username = username;
 }
+exports.setAuthenticationState = setAuthenticationState;
 // Function to check if a user is authenticated
 function checkAuthentication() {
     return globalState.isAuthenticated;
 }
+exports.checkAuthentication = checkAuthentication;
 // Function to get the username
 function getUsername() {
     return globalState.username;
 }
+exports.getUsername = getUsername;
 module.exports = {
     setAuthenticationState: setAuthenticationState,
     checkAuthentication: checkAuthentication,
